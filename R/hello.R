04ef49a708aa5c7b3d6d7137abafec5ce28b7d3d
RGreensmith <rose.greensmith@yahoo.co.uk>
2025-08-07 12:34:36 +0100

    added a cpp function inside an r function in an r file

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,16 +13,20 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-#' Hello, world!
-#'
-#' This function allows you to print 'Hello, world!'.
-#' @param  a (character) print this. 
-#' @keywords cats
-#' @export
-#' @examples
-#' hello()
-#' 
 hello <- function(a) {
   print("Hello, world!")
   print(a)
+
+  Rcpp::cppFunction('CharacterVector hello2() {
+      return "sausage";
+    }'
+  )
+  print(hello2())
 }
+
+# cppFunction('int add(int x, int y, int z) {
+#   int sum = x + y + z;
+#   return sum;
+# }')
+# add
+# add(1,2,3)
