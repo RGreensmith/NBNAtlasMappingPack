9173e452ef6968a2bb10e08731aeb6e19091235f
RGreensmith <rose.greensmith@yahoo.co.uk>
2025-08-07 14:11:55 +0100

    experimenting in the cpp only playground

--- src/cppOnlyPlayground.cpp
+++ src/cppOnlyPlayground.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+using namespace std;
+
+// Void function definition
+void hello()
+{
+    cout << "Sausages" << endl;
+}
+
+// Return-type function definition
+int square(int x)
+{
+    return x * x;
+}
+
+int divide(int x, int y)
+{
+    return x / y;
+}
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    cout << "Hello World" << endl;
+    cout << "hi" << endl;
+
+    // Calling the void function
+    hello();
+
+    // Calling the return-type function
+    int result = square(5);
+    cout << "Square of 5 is: " << result << endl;
+
+    cout << "10 divided by 2 is: " << divide(10, 2) << endl;
+
+    return 0;
     
 }
-
